92d382dbbe1cc68975de2e7caef0aeeb8c458431
cdurkin35 <cdurkin35@gmail.com>
2023-04-20 23:06:33 -0400

    Added rb file

---
+++ v0.30.0.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class V0300 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/pokerregion/poker/archive/refs/tags/v0.30.0.tar.gz"
+  sha256 "897a44f5183fd7fe80c580c6140b2896097bb3e3752233108a48f8031f9af3be"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test v0.30.0`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
